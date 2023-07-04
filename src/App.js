f4f862adde06a7e433eaa34e4b6811040eaa71f3
FlavS41 <cezarsalxz41@gmail.com>
2023-07-04 14:02:32 -0400

    Finished database and linked it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
+import { useState, useEffect } from "react";
+
 const App = () => {
+  const [quiz, setQuiz] = useState(null);
+
+  const fecthData = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/quiz");
+      const json = await response.json();
+      setQuiz(json);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect;
+
   return <div></div>;
 };
 
